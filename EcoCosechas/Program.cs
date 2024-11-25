beaf0d39a644aded13337b94e984c3fba64ded6d
cristianqsanchez <cristianquintero316@gmail.com>
2024-11-25 13:07:19 -0500

    chore: add cors

--- EcoCosechas/Program.cs
+++ EcoCosechas/Program.cs
@@ -3,9 +3,23 @@ using EcoCosechas.Models;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
+var allowOrigins = builder.Configuration.GetValue<string>("allowOrigins")!;
 
 builder.Services.AddDbContext<EcoContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddCors(opciones =>
+{
+    opciones.AddDefaultPolicy(configuracion =>
+    {
+        configuracion.WithOrigins(allowOrigins).AllowAnyHeader().AllowAnyMethod();
+    });
+
+    opciones.AddPolicy("libre", configuracion =>
+    {
+        configuracion.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod();
+    });
+});
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -22,8 +36,10 @@ app.UseHttpsRedirection();
 
 app.MapGet("/products", async (EcoContext context) =>
 {
-    var products = await context.Productos.ToListAsync();
-    return Results.Ok(products);
+    var products = await context.Productos
+    .ToListAsync();
+
+    return products;
 });
 
 app.Run();
